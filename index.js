d62b89487066654234e246cd634866fabeeddbbe
Jona Ohana <jonaohana@Jonas-Mac-Pro.local>
2025-11-09 21:46:46 -0800

    adds printer cors support

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const ALLOWED_ORIGINS = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+  "https://www.orderthevessale.com"
 ];
 
 // --------------------------
