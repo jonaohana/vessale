d7ae22e31047e19ab0626f2c6a7c333a4ff28f30
Jona Ohana <jonaohana@Jonas-Mac-Pro.local>
2026-01-07 12:22:07 -0800

    add additional CORS domain

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const ALLOWED_ORIGINS = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://www.orderthevessale.com"
+  "https://www.orderthevessale.com",
+  "https://orderthevessale.com",
+  "http://orderthevessale.com",
+  "https://orderthevessale.com"
 ];
 
 // --------------------------
